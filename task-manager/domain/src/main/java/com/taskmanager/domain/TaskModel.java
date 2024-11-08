13d06713b6f7cbf83485f9e703b50d9d2cf9abe0
augusto <augusto.faoliveira@gmail.com>
2024-11-08 17:23:33 -0300

    feat: adicao das entidades em domain

--- task-manager/domain/src/main/java/com/taskmanager/domain/TaskModel.java
+++ task-manager/domain/src/main/java/com/taskmanager/domain/TaskModel.java
@@ -1,4 +1,70 @@
 package com.taskmanager.domain;
 
+import java.util.Date;
+
 public class TaskModel {
+
+    private int id;
+    private String name;
+    private String description;
+    private Date dueDate;
+    private String priority;
+    private String status;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
--- task-manager/domain/src/main/java/com/taskmanager/domain/UserModel.java
+++ task-manager/domain/src/main/java/com/taskmanager/domain/UserModel.java
@@ -1,4 +1,58 @@
 package com.taskmanager.domain;
 
 public class UserModel {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
