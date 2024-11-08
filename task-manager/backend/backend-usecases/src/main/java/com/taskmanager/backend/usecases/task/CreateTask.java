f4933db7f4953c4a72043ba5bc98372e4ed24c89
augusto <augusto.faoliveira@gmail.com>
2024-11-08 18:12:31 -0300

    feat: adicao do caso de uso de create task

---
+++ task-manager/backend/backend-usecases/src/main/java/com/taskmanager/backend/usecases/task/CreateTask.java
@@ -0,0 +1,33 @@
+package com.taskmanager.backend.usecases.task;
+
+import com.taskmanager.backend.usecases.port.TaskRepository;
+import com.taskmanager.domain.TaskModel;
+
+public class CreateTask {
+
+    private final TaskRepository taskRepository;
+    public CreateTask(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+    public int createTask(final TaskModel taskModel){
+        if (taskModel == null){
+            return -1;
+        }
+        if (taskModel.getName().isEmpty()
+                || taskModel.getDescription().isEmpty()
+                || taskModel.getDueDate() == null
+                || taskModel.getPriority().isEmpty()
+                || taskModel.getStatus().isEmpty()){
+            return -1;
+        }
+        int id = 0;
+        try {
+            id = taskRepository.create(taskModel);
+            taskModel.setId(id);
+        }catch (Exception e){
+            return -1;
+        }
+        return id;
+    }
+
+}
