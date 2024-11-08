2868e6f4ec6e15645edc0439701b09ff6b2f1bbd
augusto <augusto.faoliveira@gmail.com>
2024-11-08 19:27:02 -0300

    feat: adicao do caso de uso de update task

---
+++ task-manager/backend/backend-usecases/src/main/java/com/taskmanager/backend/usecases/task/UpdateTask.java
@@ -0,0 +1,31 @@
+package com.taskmanager.backend.usecases.task;
+
+import com.taskmanager.backend.usecases.port.TaskRepository;
+import com.taskmanager.domain.TaskModel;
+
+public class UpdateTask {
+
+    private final TaskRepository taskRepository;
+
+    public UpdateTask(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public boolean update(final TaskModel taskModel){
+
+        final TaskModel byId = taskRepository.findById(taskModel.getId());
+        byId.setName(taskModel.getName());
+        byId.setDescription(taskModel.getDescription());
+        byId.setDueDate(taskModel.getDueDate());
+        byId.setPriority(taskModel.getPriority());
+        byId.setStatus(taskModel.getStatus());
+
+        boolean updateTask = false;
+        try {
+            updateTask = taskRepository.update(byId);
+            return updateTask;
+        } catch (Exception e){
+            return true;
+        }
+    }
+}
