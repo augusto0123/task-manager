863290f10301475843a50b9397f2beffc216f7b8
augusto <augusto.faoliveira@gmail.com>
2024-11-09 19:11:24 -0300

    feat: adicao do account controller no frontend

---
+++ task-manager/frontend/frontend-springtm/src/main/java/com/taskmanager/frontendspringtm/controller/AccountController.java
@@ -0,0 +1,20 @@
+package com.taskmanager.frontendspringtm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "account/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(){
+        return "account/register";
+    }
+}
