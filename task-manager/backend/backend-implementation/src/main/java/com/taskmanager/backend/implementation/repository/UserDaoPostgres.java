562bbb3faa8d0c75bc805988af5242900664168d
augusto <augusto.faoliveira@gmail.com>
2024-11-09 11:24:27 -0300

    feat: adicao do metodo de find all no user dao postgres

--- task-manager/backend/backend-implementation/src/main/java/com/taskmanager/backend/implementation/repository/UserDaoPostgres.java
+++ task-manager/backend/backend-implementation/src/main/java/com/taskmanager/backend/implementation/repository/UserDaoPostgres.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoPostgres implements UserRepository {
@@ -51,7 +52,40 @@ public class UserDaoPostgres implements UserRepository {
 
     @Override
     public List<UserModel> findAll() {
-        return null;
+
+        final List<UserModel> userModels = new ArrayList<>();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        final String sql = "SELECT * FROM usuario;";
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                final UserModel userModel = new UserModel();
+                userModel.setId(resultSet.getInt("id"));
+                userModel.setName(resultSet.getString("nome"));
+                userModel.setEmail(resultSet.getString("email"));
+                userModel.setPassword(resultSet.getString("senha"));
+                userModel.setPasswordConfirm(resultSet.getString("confirmar_senha"));
+                userModel.setType(resultSet.getString("tipo"));
+
+                userModels.add(userModel);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return userModels;
     }
 
     @Override
