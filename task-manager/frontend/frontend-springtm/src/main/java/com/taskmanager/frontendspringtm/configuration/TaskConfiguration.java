ebf1257d9f570525ef8cb2484c3ff154f42c6f2a
augusto <augusto.faoliveira@gmail.com>
2024-11-09 15:37:26 -0300

    feat: adicao do task configuration no frontend springtm

---
+++ task-manager/frontend/frontend-springtm/src/main/java/com/taskmanager/frontendspringtm/configuration/TaskConfiguration.java
@@ -0,0 +1,40 @@
+package com.taskmanager.frontendspringtm.configuration;
+
+import com.taskmanager.domain.TaskModel;
+import com.taskmanager.frontend.usecases.port.RestService;
+import com.taskmanager.frontend.usecases.task.*;
+import com.taskmanager.frontendspringtm.port.impl.RestApiController;
+import org.springframework.context.annotation.Bean;
+
+public class TaskConfiguration {
+
+    @Bean
+    public CreateTask createTask(){
+        RestService<TaskModel> restService = new RestApiController<>();
+        return new CreateTask(restService);
+    }
+
+    @Bean
+    public FindAllTasks findAllTasks(){
+        RestService<TaskModel> restService = new RestApiController<>();
+        return new FindAllTasks(restService);
+    }
+
+    @Bean
+    public FindTaskById findTaskById(){
+        RestService<TaskModel> restService = new RestApiController<>();
+        return new FindTaskById(restService);
+    }
+
+    @Bean
+    public UpdateTask updateTask(){
+        RestService<TaskModel> restService = new RestApiController<>();
+        return new UpdateTask(restService);
+    }
+
+    @Bean
+    public DeleteTask deleteTask(){
+        RestService<TaskModel> restService = new RestApiController<>();
+        return new DeleteTask(restService);
+    }
+}
