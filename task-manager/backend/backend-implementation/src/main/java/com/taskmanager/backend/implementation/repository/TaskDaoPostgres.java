8284308797d217ba64dfa9c4cb214a7196c69323
augusto <augusto.faoliveira@gmail.com>
2024-11-10 16:11:50 -0300

    feat: adicao do find by status no task dao postgres

--- task-manager/backend/backend-implementation/src/main/java/com/taskmanager/backend/implementation/repository/TaskDaoPostgres.java
+++ task-manager/backend/backend-implementation/src/main/java/com/taskmanager/backend/implementation/repository/TaskDaoPostgres.java
@@ -88,7 +88,45 @@ public class TaskDaoPostgres implements TaskRepository {
 
     @Override
     public List<TaskModel> findByStatus(String type) {
-        return null;
+        final List<TaskModel> taskModels = new ArrayList<>();
+
+        if (type == null || type.isEmpty()) {
+            throw new IllegalArgumentException("O status não pode ser nulo ou vazio.");
+        }
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        final String sql = "SELECT * FROM tarefa WHERE status = ?;";
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, type);
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                final TaskModel taskModel = new TaskModel();
+                taskModel.setId(resultSet.getInt("id"));
+                taskModel.setName(resultSet.getString("nome"));
+                taskModel.setDescription(resultSet.getString("descricao"));
+                taskModel.setDueDate(resultSet.getString("data_vencimento"));
+                taskModel.setPriority(resultSet.getString("prioridade"));
+                taskModel.setStatus(resultSet.getString("status"));
+
+                taskModels.add(taskModel);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return taskModels;
     }
 
     @Override
