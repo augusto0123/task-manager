013a7e1703642caacf470689baea3d58f23c10e4
augusto <augusto.faoliveira@gmail.com>
2024-11-10 16:08:04 -0300

    feat: adicao do caso de uso de find by status no find task em backend usecases

--- task-manager/backend/backend-usecases/src/main/java/com/taskmanager/backend/usecases/task/FindTask.java
+++ task-manager/backend/backend-usecases/src/main/java/com/taskmanager/backend/usecases/task/FindTask.java
@@ -4,6 +4,7 @@ import com.taskmanager.backend.usecases.exception.InvalidException;
 import com.taskmanager.backend.usecases.exception.NotFoundException;
 import com.taskmanager.backend.usecases.port.TaskRepository;
 import com.taskmanager.domain.TaskModel;
+import org.springframework.scheduling.config.Task;
 
 import java.util.List;
 
@@ -33,5 +34,19 @@ public class FindTask {
             throw new NotFoundException(message);
         }
         return taskModel;
+    }
+
+    public List<TaskModel> findByStatus(final String status){
+        if (status == null || status.isEmpty()) {
+            throw new InvalidException();
+        }
+
+        final List<TaskModel> taskModels = taskRepository.findByStatus(status);
+
+        if (taskModels == null || taskModels.isEmpty()){
+            final String message = "Nenhuma tarefa com o status: (" + status + ") foi encontrada";
+            throw new NotFoundException(message);
+        }
+        return taskModels;
     }
 }
